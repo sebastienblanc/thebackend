53d7b20dbfafb78f9eb936a080bafd73b94721a0
Sebastien Blanc <scm.blanc@gmail.com>
2014-09-25 11:08:54 +0200

    first commit

---
+++ src/main/java/org/thebackend/rest/TaskEndpoint.java
@@ -0,0 +1,104 @@
+package org.thebackend.rest;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.OptimisticLockException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.UriBuilder;
+import org.thebackend.model.Task;
+
+/**
+ * 
+ */
+@Stateless
+@Path("/tasks")
+public class TaskEndpoint
+{
+   @PersistenceContext(unitName = "thebackend-persistence-unit")
+   private EntityManager em;
+
+   @POST
+   @Consumes("application/json")
+   public Response create(Task entity)
+   {
+      em.persist(entity);
+      return Response.created(UriBuilder.fromResource(TaskEndpoint.class).path(String.valueOf(entity.getId())).build()).build();
+   }
+
+   @DELETE
+   @Path("/{id:[0-9][0-9]*}")
+   public Response deleteById(@PathParam("id") Long id)
+   {
+      Task entity = em.find(Task.class, id);
+      if (entity == null)
+      {
+         return Response.status(Status.NOT_FOUND).build();
+      }
+      em.remove(entity);
+      return Response.noContent().build();
+   }
+
+   @GET
+   @Path("/{id:[0-9][0-9]*}")
+   @Produces("application/json")
+   public Response findById(@PathParam("id") Long id)
+   {
+      TypedQuery<Task> findByIdQuery = em.createQuery("SELECT DISTINCT t FROM Task t WHERE t.id = :entityId ORDER BY t.id", Task.class);
+      findByIdQuery.setParameter("entityId", id);
+      Task entity;
+      try
+      {
+         entity = findByIdQuery.getSingleResult();
+      }
+      catch (NoResultException nre)
+      {
+         entity = null;
+      }
+      if (entity == null)
+      {
+         return Response.status(Status.NOT_FOUND).build();
+      }
+      return Response.ok(entity).build();
+   }
+
+   @GET
+   @Produces("application/json")
+   public List<Task> listAll(@QueryParam("start") Integer startPosition, @QueryParam("max") Integer maxResult)
+   {
+      TypedQuery<Task> findAllQuery = em.createQuery("SELECT DISTINCT t FROM Task t ORDER BY t.id", Task.class);
+      if (startPosition != null)
+      {
+         findAllQuery.setFirstResult(startPosition);
+      }
+      if (maxResult != null)
+      {
+         findAllQuery.setMaxResults(maxResult);
+      }
+      final List<Task> results = findAllQuery.getResultList();
+      return results;
+   }
+
+   @PUT
+   @Path("/{id:[0-9][0-9]*}")
+   @Consumes("application/json")
+   public Response update(Task entity)
+   {
+      try
+      {
+         entity = em.merge(entity);
+      }
+      catch (OptimisticLockException e)
+      {
+         return Response.status(Response.Status.CONFLICT).entity(e.getEntity()).build();
+      }
+
+      return Response.noContent().build();
+   }
+}
